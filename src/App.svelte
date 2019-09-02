2cc269d850799fc2ef325413603119131d7fadf3
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-02 10:16:33 -0400

    Adding some dummy data and working on component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script>
   import Header from './UI/Header.svelte';
+
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Meetup',
+      subtitle: 'Coding... oh what fun',
+      description: 'Come hang out with some really popular coders',
+      imageUrl:
+      'https://pixabay.com/photos/workplace-team-business-meeting-1245776/',
+      address: '27th Nerd Road, 32523 New Youk',
+      contactEmail: 'code@test.com'
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let\'s go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl: 'https://pixabay.com/photos/swimmer-sport-swim-water-crawl-1678307/',
+      address: '27th Nerd Road, 32523 New Youk',
+      contactEmail: 'swim@test.com'
+    },
+  ]
 </script>
 
 <Header />
