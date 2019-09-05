4074ae13ce5ad7f214645e8c6d1bbed83516842a
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-05 10:21:24 -0400

    Moving form out of App

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,7 @@
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
   import TextInput from './UI/TextInput.svelte';
   import Button from './UI/Button.svelte';
-
-  let title = "";
-  let subtitle = "";
-  let address = "";
-  let email = "";
-  let description = "";
-  let imageUrl = "";
+  import EditMeetup from './Meetups/EditMeetup.svelte';
 
   let meetups = [
     {
@@ -33,6 +27,8 @@
       isFavorite: false
     },
   ]
+
+  let editMode;
 
   function addMeetup() {
     const newMeetup = {
@@ -62,58 +58,15 @@
   main {
     margin-top: 5rem;
   }
-
-  form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
-  }
 </style>
 
 <Header />
 
 <main>
-  <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-      id="title"
-      label="Title"
-      value={title}
-      on:input={event => title = event.target.value}
-      />
-    <TextInput
-      id="subtitle"
-      label="Subtitle"
-      value={subtitle}
-      on:input={event => subtitle = event.target.value}
-      />
-    <TextInput
-      id="address"
-      label="Address"
-      value={address}
-      on:input={event => address = event.target.value}
-      />
-    <TextInput
-      id="imageUrl"
-      label="Image URL"
-      value={imageUrl}
-      on:input={event => imageUrl = event.target.value}
-      />
-    <TextInput
-      id="email"
-      label="E-Mail"
-      value={email}
-      type="email"
-      on:input={event => email = event.target.value}
-      />
-    <TextInput
-      id="description"
-      label="Description"
-      value={description}
-      controlType="textarea"
-      on:input={event => description = event.target.value}
-      />
-      <Button type="submit" caption="Save" />
-  </form>
+  <Button caption="New Meetup"  on:click={() => editMode = 'add'}/>
+  {#if editMode === 'add'}
+    <EditMeetup />
+  {/if}
   <MeetupGrid
     {meetups}
     on:togglefavorite={toggleFavorite}
