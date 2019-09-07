a66573d289c7d7d1ad47c487eec5973d105c20c2
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-07 13:06:41 -0400

    using custom store to deal with meetups

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,23 @@
 <script>
+  import meetups from './Meetups/meetups-store.js';
   import Header from './UI/Header.svelte';
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
   import TextInput from './UI/TextInput.svelte';
   import Button from './UI/Button.svelte';
   import EditMeetup from './Meetups/EditMeetup.svelte';
 
-  let meetups = [
-    {
-      id: 'm1',
-      title: 'Coding Meetup',
-      subtitle: 'Coding... oh what fun',
-      description: 'Come hang out with some really popular coders',
-      imageUrl: 'https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_1280.jpg',
-      address: '27th Nerd Road, 32523 New Youk',
-      contactEmail: 'code@test.com',
-      isFavorite: false
-    },
-    {
-      id: 'm2',
-      title: 'Swim Together',
-      subtitle: 'Let\'s go for some swimming',
-      description: 'We will simply swim some rounds!',
-      imageUrl: 'https://cdn.pixabay.com/photo/2016/09/18/14/21/swimmer-1678307_1280.jpg',
-      address: '27th Nerd Road, 32523 New Youk',
-      contactEmail: 'swim@test.com',
-      isFavorite: false
-    },
-  ]
-
   let editMode;
 
   function addMeetup(event) {
-    const newMeetup = {
-      id: Math.random(),
+    const meetupData = {
       title: event.detail.title,
       subtitle: event.detail.subtitle,
       description: event.detail.description,
-      address: event.detail.address,
       imageUrl: event.detail.imageUrl,
-      email: event.detail.email
+      email: event.detail.email,
+      address: event.detail.address
     }
-    meetups = [newMeetup, ...meetups]
+    meetups.addMeetup(meetupData);
     editMode = null;
   }
 
@@ -50,12 +27,7 @@
 
   function toggleFavorite(event) {
     const id = event.detail;
-    const updatedMeetup = {...meetups.find(m => m.id === id) }
-    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
-    const meetupIndex = meetups.findIndex(m => m.id === id);
-    const updatedMeetups = [...meetups]
-    updatedMeetups[meetupIndex] = updatedMeetup;
-    meetups = updatedMeetups;
+    meetups.toggleFavorite(id);
   }
 </script>
 
@@ -79,7 +51,7 @@
     <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
   {/if}
   <MeetupGrid
-    {meetups}
+    meetups={$meetups}
     on:togglefavorite={toggleFavorite}
     />
 </main>
---
+++ src/Meetups/meetups-store.js
@@ -0,0 +1,52 @@
+import { writable } from 'svelte/store';
+
+const meetups = writable(
+[
+    {
+      id: 'm1',
+      title: 'Coding Meetup',
+      subtitle: 'Coding... oh what fun',
+      description: 'Come hang out with some really popular coders',
+      imageUrl: 'https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_1280.jpg',
+      address: '27th Nerd Road, 32523 New Youk',
+      email: 'code@test.com',
+      isFavorite: false
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let\'s go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl: 'https://cdn.pixabay.com/photo/2016/09/18/14/21/swimmer-1678307_1280.jpg',
+      address: '27th Nerd Road, 32523 New Youk',
+      email: 'swim@test.com',
+      isFavorite: false
+    },
+  ]
+);
+
+const customMeetupsStore = {
+	subscribe: meetups.subscribe,
+	addMeetup: (meetupData) => {
+		const newMeetup = {
+			...meetupData,
+			id: Math.random().toString(),
+			isFavorite: false
+		}
+		meetups.update(items => {
+			return [newMeetup, ...items]
+		})
+	},
+	toggleFavorite: (id) => {
+		meetups.update(items => {
+			const updatedMeetup = {...items.find(m => m.id === id) }
+			updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+			const meetupIndex = items.findIndex(m => m.id === id);
+			const updatedMeetups = [...items]
+			updatedMeetups[meetupIndex] = updatedMeetup;
+			return updatedMeetups;
+		})
+	}
+}
+
+export default customMeetupsStore;
